4b7b8f8c716eb376a77ac97547aab77f26cd8dff
Kathiravan Harikrishnan <kathiravan.iosdev@gmail.com>
2025-05-25 12:30:15 +0530

    V.2

--- CICDProcess/ContentView.swift
+++ CICDProcess/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world! V.1")
+            Text("Hello, world! V.2")
         }
         .padding()
     }
